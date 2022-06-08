612777c697d3b2ecf0c4b0f61f2f676af891c029
1107698728 <106578175+1107698728@users.noreply.github.com>
2022-06-08 23:46:39 +0800

    Update run_case.py

--- run_case.py
+++ run_case.py
@@ -2,10 +2,12 @@ import unittest
 from HTMLTestRunner import HTMLTestRunner
 from api.base import Base
 
+
+test_report="test_report.html"
 if __name__=="__main__":
     base=Base()
     base.login()
     suite=unittest.TestLoader().discover("cases",pattern="test*.py")
-    with open("test_report","wb") as f:
+    with open(test_report,"wb") as f:
         runner= HTMLTestRunner(f,title="标题",description="测试用户")
         runner.run(suite)
